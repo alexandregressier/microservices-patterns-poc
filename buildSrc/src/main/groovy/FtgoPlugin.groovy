cc72f25bc269f0cd70dd783f57b3047165b3b4c9
Alexandre Gressier <alexandre@gressier.dev>
2021-03-07 15:42:02 +0100

    Rewrite the FTGO Gradle plugin in Groovy

---
+++ buildSrc/src/main/groovy/FtgoPlugin.groovy
@@ -0,0 +1,49 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FtgoPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.with {
+            apply plugin: 'org.jetbrains.kotlin.jvm'
+            apply plugin: 'org.jetbrains.kotlin.plugin.spring'
+            apply plugin: 'org.springframework.boot'
+            apply plugin: 'io.spring.dependency-management'
+
+            group = 'dev.foodtogo'
+            java.sourceCompatibility = JavaVersion.VERSION_11
+
+            repositories {
+                jcenter()
+            }
+
+            dependencies {
+                // Kotlin
+                implementation 'org.jetbrains.kotlin:kotlin-stdlib-jdk8'
+                implementation 'org.jetbrains.kotlin:kotlin-reflect'
+
+                // Testing
+                testImplementation 'org.springframework.boot:spring-boot-starter-test'
+            }
+
+            dependencyManagement {
+                imports {
+                    mavenBom "org.springframework.cloud:spring-cloud-dependencies:${ext.versionBomSpringCloud}"
+                }
+            }
+
+            tasks.withType(compileKotlin.class) {
+                kotlinOptions {
+                    freeCompilerArgs = ['-Xjsr305=strict']
+                    jvmTarget = java.sourceCompatibility
+                }
+            }
+
+            test {
+                useJUnitPlatform()
+            }
+        }
+    }
+}
--- ftgo-commons/build.gradle.kts
+++ ftgo-commons/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
@@ -7,16 +6,9 @@ plugins {
 }
 
 version = "0.1.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
 dependencies {}
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "${java.sourceCompatibility}"
-    }
-}
 tasks.withType<BootJar> {
     enabled = false
 }

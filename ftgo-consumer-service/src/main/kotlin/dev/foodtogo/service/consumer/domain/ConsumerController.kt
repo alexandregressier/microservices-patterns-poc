5a80722c13dd53f1baadb1993a2c2a8cedd6222d
Alexandre Gressier <alexandre@gressier.dev>
2021-03-15 23:53:21 +0100

    Create the POST Consumers & GET Consumer endpoints

--- ftgo-consumer-service/src/main/kotlin/dev/foodtogo/service/consumer/domain/ConsumerController.kt
+++ ftgo-consumer-service/src/main/kotlin/dev/foodtogo/service/consumer/domain/ConsumerController.kt
@@ -1,9 +1,9 @@
 package dev.foodtogo.service.consumer.domain
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/consumers")
@@ -11,6 +11,16 @@ class ConsumerController {
 
     @Autowired private lateinit var consumerRepository: ConsumerRepository
 
+    @PostMapping
+    fun postConsumers(@RequestBody consumer: Consumer): Consumer =
+        consumerRepository.save(consumer)
+
+    @GetMapping("/{id}")
+    fun getConsumer(@PathVariable id: ConsumerId): Consumer =
+        consumerRepository.findById(id)
+            .orElseThrow { throw ResponseStatusException(HttpStatus.NOT_FOUND) }
+
     @GetMapping
-    fun getConsumers(): List<Consumer> = consumerRepository.findAll()
+    fun getConsumers(): List<Consumer> =
+        consumerRepository.findAll()
 }

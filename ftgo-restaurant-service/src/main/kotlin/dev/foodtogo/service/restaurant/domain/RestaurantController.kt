5c3fe68679c81ad3024f64c5737061223afc2b46
Alexandre Gressier <alexandre@gressier.dev>
2021-03-15 23:53:48 +0100

    Create the POST Restaurants & GET Restaurant endpoints

--- ftgo-restaurant-service/src/main/kotlin/dev/foodtogo/service/restaurant/domain/RestaurantController.kt
+++ ftgo-restaurant-service/src/main/kotlin/dev/foodtogo/service/restaurant/domain/RestaurantController.kt
@@ -1,9 +1,9 @@
 package dev.foodtogo.service.restaurant.domain
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/restaurants")
@@ -11,6 +11,16 @@ class RestaurantController {
 
     @Autowired private lateinit var restaurantRepository: RestaurantRepository
 
+    @PostMapping
+    fun postRestaurants(@RequestBody restaurant: Restaurant): Restaurant =
+        restaurantRepository.save(restaurant)
+
+    @GetMapping("/{id}")
+    fun getRestaurant(@PathVariable id: RestaurantId): Restaurant =
+        restaurantRepository.findById(id)
+            .orElseThrow { throw ResponseStatusException(HttpStatus.NOT_FOUND) }
+
     @GetMapping
-    fun getRestaurants(): List<Restaurant> = restaurantRepository.findAll()
+    fun getRestaurants(): List<Restaurant> =
+        restaurantRepository.findAll()
 }

9d21c7e3e7ea0c59eb2693afffbb1e7c6d6917b5
Alexandre Gressier <alexandre@gressier.dev>
2021-03-15 15:48:14 +0100

    Remove useless blank lines

--- ftgo-commons/src/main/kotlin/dev/foodtogo/commons/PhoneNumber.kt
+++ ftgo-commons/src/main/kotlin/dev/foodtogo/commons/PhoneNumber.kt
@@ -8,9 +8,7 @@ import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
 class PhoneNumber @JsonCreator constructor(phoneNumber: String) : Phonenumber.PhoneNumber() {
-
     init { mergeFrom(PhoneNumberUtil.getInstance().parse(phoneNumber, "$countryCodeSource")) }
-
     @JsonValue override fun toString(): String =
         PhoneNumberUtil.getInstance().format(this, PhoneNumberUtil.PhoneNumberFormat.E164)
 }
